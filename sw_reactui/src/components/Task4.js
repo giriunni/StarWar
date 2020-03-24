6cd69f73782b2b4a12d27df8f75315556f9f7b0b
Girish Unni <62099480+giriunni@users.noreply.github.com>
2020-03-24 17:04:30 +0530

    Update Task4.js
    Empty Template for Task4 component...As this branch is for Task1 development

--- sw_reactui/src/components/Task4.js
+++ sw_reactui/src/components/Task4.js
@@ -1,32 +1,11 @@
 import React, {Component} from 'react';
 
 export class Task4 extends Component{
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-            taskdata: ""  
-        };
-    }
-
-    componentDidMount(){
-        this.getAPIData();
-    }
-
-    getAPIData()
-    {
-        fetch('http://localhost:44382/api/starwarfilm/GetLargestNumberOfVehiclePilot')
-        .then(response => response.json())
-        .then(data => {
-            this.setState({taskdata: data})
-            }
-        )
-    }
+   
     render(){
         return(
             <div className = "newline-data">
-                <h4><strong>What planet in Star Wars universe provided largest number of vehicle pilots?</strong></h4>
-                <h4 style={{color: "yellow"}}><strong >{this.state.taskdata}</strong></h4>                
+                    
             </div>            
         )
     }
