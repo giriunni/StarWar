608e8d637f1114a139ed88f3ada7f7b6afe6e442
Girish Unni <62099480+giriunni@users.noreply.github.com>
2020-03-24 17:15:46 +0530

    Update Task3.js
    Empty Template for Task3 component...As this branch is for Task4 development

--- sw_reactui/src/components/Task3.js
+++ sw_reactui/src/components/Task3.js
@@ -2,32 +2,11 @@ import React, {Component} from 'react';
 
 
 export class Task3 extends Component{
-    constructor(props) {
-        super(props);
     
-        this.state = {
-            taskdata: ""  
-        };
-    }
-
-    componentDidMount(){
-        this.getAPIData();
-    }
-
-    getAPIData()
-    {
-        fetch('http://localhost:44382/api/starwarfilm/GetMostAppearedSpeciesWithCount')
-        .then(response => response.json())
-        .then(data => {
-            this.setState({taskdata: data})
-            }
-        )
-    }
     render(){
         return(
             <div className = "newline-data">
-                <h4><strong>What species appeared in the most number of Star Wars films?</strong></h4>
-                <h4 style={{color: "yellow"}}><strong >{this.state.taskdata}</strong></h4>                
+                
             </div>            
         )
     }
